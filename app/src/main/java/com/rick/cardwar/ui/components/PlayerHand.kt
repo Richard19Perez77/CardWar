d5655f2b1fd59e3bd3a99aaca22bcde5d250a653
richardanthony.perez@external.cnh.com <richardanthony.perez@external.cnh.com>
2026-08-30 05:16:01 +0700

    layout changes

--- app/src/main/java/com/rick/cardwar/ui/components/PlayerHand.kt
+++ app/src/main/java/com/rick/cardwar/ui/components/PlayerHand.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.BoxWithConstraintsScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
